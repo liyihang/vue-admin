112b437e1f814f96ae85cf319e37ebe577316a43
liyihang <lidoudoou@gmail.com>
2019-07-30 21:03:02 +0800

    start

---
+++ src/components/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login-container">
+    <div class="login-row">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style scoped lang="less">
+.login-container{
+  height:100%;
+  width: 100%;
+  background-color:lightcyan;
+}
+.login-row {
+  height: 400px;
+  width: 350px;
+  background-color: aliceblue;
+}
+</style>
